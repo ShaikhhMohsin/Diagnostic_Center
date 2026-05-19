0448e7ed69b1aeb80739bda2bd031013687b8387
Shaikh Mohsin <shadmosin@gmail.com>
2026-05-19 09:39:42 +0530

    Fixed turbopack root mismatch

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.join(__dirname),
   turbopack: {
-    root: __dirname,
+    root: path.join(__dirname),
   },
 };
 
